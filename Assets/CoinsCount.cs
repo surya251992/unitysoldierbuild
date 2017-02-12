7348edd3c70d88dc43697fa2d051d538256aa0bc
surya prakash <surya251992@gmail.com>
2017-02-12 17:29:00 +0530

    Coins section added

---
+++ Assets/CoinsCount.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CoinsCount : MonoBehaviour {
+	public Text TotalCoins;
+	// Use this for initialization
+	void Start () {
+
+		TotalCoins.text = "Coins Available:  "+PlayerPrefs.GetInt("Coins").ToString();
+	
+	}
+}
